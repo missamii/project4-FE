9db6e2acf4bb51d7f8e601af79c949fc866edf76
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2016-08-29 15:01:56 -0400

    about page and readme file upated

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # About the App
+
+A car shopping App. Created by Carrow and Tiffany. It was built in a matter of 7 days for a final project at General Assembly. This app is mean't to be used to aid people in vehicle shopping. Making the process more of a relaxed experience.
+
 stuff about the app, what does it do? How to use it? Why that name?
 
+
 ## Documentation
+This app is built on React (front-end) and Ruby on Rails (back-end). The front end is coded in ES6 and the back end is coded in Ruby. With a SQLite Database that supports a Message Board and other CRUD(Create, Retrieve, Update, Delete) features.
+
 photos? built on? what gems? bootstrap or something else?
 
 
@@ -11,8 +17,35 @@ put info here
 
 
 Tiffany
-put info here
+put info here. maybe shameless plug to linkedin or blog
 
 
 #### Credits
-put all links and special thanks here
+[put all links and special thanks here]
+
+Thanks to Mimi Wu for helping Tiffany go about a better way to create the backend and frontend repos.
+
+**Images:**
++ [Google](https://www.google.com)
+
+**Programs:**
++ [Corel](https://www.corel.com)
++ [Atom](https://www.corel.com) &  [Sublime](https://www.corel.com)
+
+
+**Resources:**
++ [Bootstrap](https://www.getbootstrap.com)
+
+**Features/Code:**
++ [Ruby on Rails](http://rubyonrails.org/)
++ [Ruby](https://www.ruby-lang.org/en/)
++ [ECMASript](http://es6-features.org/)
++ [SQLite](https://sqlite.org/)
++ [axios](https://www.npmjs.com/package/axios)
++ [React](https://facebook.github.io/react/)
+
+
+
+
+**what else did we use?:**
++ [I'm an inline-style link](https://www.google.com)
--- src/Component/About.js
+++ src/Component/About.js
@@ -7,8 +7,25 @@ class About extends Component {
     return (
       <div>
         <Header />
-        <h1>About Page</h1>
+        <h1>About [Name of App here]</h1>
+        {/* started on the about page, will need to add pictures of creators, and so */}
+          <p>
+            This App out of many, was created for our final project. We wanted to make car shopping easier and more relaxed. This App brings your current and relevant information about the car of your dreams. You can shop different dealerships near you, find a vehicle by the make, model, year or the reviews. Use Reviews to find out what other people are saying about a particular vehicle or the best dealership to shop.
+          </p>
 
+        <h3>Carrow Thibault</h3>
+          <p>
+            A WDI (web development immersive) Student at General Assembly. [talk about a few hobbies, what do you like to code in most? any goals?]
+          </p>
+
+        <h3>Tiffany Lo</h3>
+          <p>
+            A WDI (web development immersive) Student at General Assembly. She found her love for design and coding at 12 years old. Stopped by a 7 year haitus from everything she loved. GA has helped rekindle her most beloved passions. Some of her favorite technologies are Wordpress, CSS, Hugo & Ruby.
+          </p>
+          <p>
+            <strike>She is all about code, all day, everyday and does nothing else.</strike> If she's actually not behind a computer, Tiffany enjoys the beach and is also known as <b>beach bum</b>. She loves being out doors and has been called a <u>sun child</u> on many occasions, dispite her battle with <u>Lupus SLE</u>. In the future she'll be applying her talents to open sourced projects to work with Non-profits for the betterment of others. Her dream job is working as Developer at either <a href="rainn.org">RAINN.org</a> or <a href="safehorizon.org">Safe Horizon</a>
+
+          </p>
       </div>
       )
   }
