9a99949456fafe8aa2423e8d92ff775a1510e5fd
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2016-08-29 20:11:05 -0400

    bootstrap foolishness

---
+++ src/Component/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from  'react';
+import '../css/Footer.css';
+
+class Footer extends Component {
+
+  render() {
+    return(
+      <div className="Footer">
+        &copy; 2016 Carrow & Tiffany Lo
+      </div>
+    )
+  }
+}
+
+export default Footer
