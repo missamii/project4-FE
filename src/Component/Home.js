018fa89f7d75424767fe0ddf94b44779eed34f81
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2016-09-01 17:48:00 -0400

    home js

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -9,12 +9,8 @@ class Home extends Component {
   render() {
     return (
       <div className="wrapper">
-<<<<<<< HEAD
         <Jumbotron className="Home">
           <h1>Welcome [APP NAME]</h1>
-=======
-
->>>>>>> master
           <p>
             <iframe className="iframe" id="ytplayer" type="text/html" width="940" height="590" frameBorder="70px"
         src="https://www.youtube.com/embed/wT-4nESh-4Y?autoplay=1&controls=0&showinfo=0"
