6262940bd6ad88f285554daea870d971d07e058d
carrow thibault <carrow1007@gmail.com>
2016-09-01 18:50:02 -0400

    adding

--- Server.js
+++ Server.js
@@ -19,6 +19,6 @@ app.get('*', (req, res) =>
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 )
 
-app.listen(process.env.PORT || 3001, () =>
+app.listen(process.env.PORT || 8080, () =>
   console.log('example app listening')
 )
